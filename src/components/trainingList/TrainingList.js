35ac4d4bc398dcf7f5cf01b162df76104353633c
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-15 12:14:38 +0200

    training time formatted with moment

--- src/components/trainingList/TrainingList.js
+++ src/components/trainingList/TrainingList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import moment from 'moment';
 
 function CustomerList() {
 
@@ -24,29 +25,28 @@ function CustomerList() {
 
   const columns = [
     {
-      Header: 'Data',
-      accessor: 'date',
-    },
-    {
-      Header: 'Duration',
-      accessor: 'duration',
-    },
-    {
       Header: 'Activity',
       accessor: 'activity',
     },
     {
-      Header: 'Customer',
+      Header: 'Duration (min)',
+      accessor: 'duration',
+    },
+    {
+      Header: 'Customer name',
       id: 'firstname_lastname',
       accessor: c => `${c.customer.firstname} ${c.customer.lastname}`,
-    }
+    },
+    {
+      Header: 'Date and time',
+      id: 'time',
+      accessor: t => moment(t.date).format('LLL')
+    },
   ];
 
   return (
     <div className="customerList">
-
       <ReactTable data={trainings} columns={columns} filterable={true}/>
-
     </div>
   );
 }
