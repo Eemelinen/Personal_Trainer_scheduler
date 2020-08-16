30b916cba1eaeaea95d095f262868326d112fee4
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-16 17:14:51 +0200

    Trainings fetched and rendered

--- src/components/calender/TrainingCalender.js
+++ src/components/calender/TrainingCalender.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
-import events from './mockEvents';
+// import events from './mockEvents';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment)
 
-const TrainingCalendar = props => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-)
+const TrainingCalendar = props => {
+  const [ events, setEvents ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    console.log('Calender component mounted');
+    console.log('Calender props', props);
+
+    const parsedEvents = props.events.map(event => {
+      return {
+        'title': event.activity,
+        'start': new Date(2020, 7, 20, 7, 0, 0),
+        'end': new Date(2020, 7, 20, 10, 30, 0),
+      }
+    });
+
+    setEvents(parsedEvents);
+    setLoading(false);
+  }, [props]);
+
+  return(
+    <div>
+      {loading
+        ?<h1>Loading</h1>
+        : <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }} />}
+    </div>
+  );
+}
+
 
 export default TrainingCalendar;
--- src/pages/Calender.js
+++ src/pages/Calender.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import TrainingCalender from '../components/calender/TrainingCalender';
 
-const Calendar = props => (
-  <div>
-    <h1>Training Calender</h1>
+const Calendar = props => {
+  const [ trainings, setTrainings ] = useState([]);
+
+  useEffect(() => {
+    // console.log('Training list loaded');
+    getTrainings();
+  }, []);
+
+  useEffect(() => {
+    // console.log(trainings);
+  }, [trainings]);
+
+  const getTrainings = () => {
+    fetch('https://customerrest.herokuapp.com/gettrainings')
+      .then(res => res.json())
+      .then(data => {
+        setTrainings(data);
+        // console.log(data);
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (
     <div>
-      <TrainingCalender />
+      <h1>Training Calender</h1>
+      <div>
+        <TrainingCalender events={trainings} />
+      </div>
     </div>
-  </div>
-)
+  );
+}
 
 export default Calendar;
